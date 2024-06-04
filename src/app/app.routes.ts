cfa4450d74a46e46761842a62746d57699b392db
fal4782 <falguni4782@gmail.com>
2024-06-04 12:21:47 +0530

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AgGridComponent } from './ag-grid/ag-grid.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AgGridComponent },
+  { path: 'form', component: DynamicFormComponent },
+];
