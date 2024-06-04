b3ebcb154c7dc20c35637ed466c70d8133fb0223
fal4782 <falguni4782@gmail.com>
2024-06-04 13:16:20 +0530

    p-table routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AgGridComponent } from './ag-grid/ag-grid.component';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { PTableComponent } from './p-table/p-table.component';
 
 export const routes: Routes = [
   { path: '', component: AgGridComponent },
   { path: 'form', component: DynamicFormComponent },
+  { path: 'p-table', component: PTableComponent },
 ];
