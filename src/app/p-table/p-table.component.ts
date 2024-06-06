6fa9164e5b3ca76cb61835053097d6900103b876
fal4782 <falguni4782@gmail.com>
2024-06-06 11:28:12 +0530

    updates

--- src/app/p-table/p-table.component.ts
+++ src/app/p-table/p-table.component.ts
@@ -1,52 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { tableData } from './table-config';
+import { CommonModule } from '@angular/common';
+
+import tableJson from './table-config.json';
 
 import { ToggleButtonModule } from 'primeng/togglebutton';
 import { TableModule } from 'primeng/table';
 import { FormsModule } from '@angular/forms';
 
-export interface Country {
-  name?: string;
-  code?: string;
-}
-
-export interface Representative {
-  name?: string;
-  image?: string;
-}
-
-export interface Customer {
-  id?: number;
-  name?: string;
-  country?: Country;
-  company?: string;
-  date?: string | Date;
-  status?: string;
-  activity?: number;
-  representative?: Representative;
-  verified?: boolean;
-  balance?: number;
-}
 
 @Component({
   selector: 'app-p-table',
   standalone: true,
-  imports: [ToggleButtonModule, TableModule, FormsModule],
+  imports: [ToggleButtonModule, TableModule, FormsModule, CommonModule],
   templateUrl: './p-table.component.html',
   styleUrls: ['./p-table.component.scss'],
 })
 export class PTableComponent implements OnInit {
   balanceFrozen = false;
   statusFrozen = false;
-  customers: Customer[] = [];
+
+  columns: any[] = [];
+  records: any[] = [];
 
   ngOnInit(): void {
-    this.loadToggleStates();
     this.getTableData();
   }
 
   getTableData(): void {
-    this.customers = tableData;
+    let storedFrozenStatus = JSON.parse(localStorage.getItem('frozenStatus') || '{}');
+    this.records = tableJson.records;
+    this.columns = tableJson.headerConfig.map((col, index) => {
+      let frozen = false;
+      if (storedFrozenStatus[index] !== undefined) {
+        frozen = storedFrozenStatus[index];
+      }
+
+      return {
+        ...col,
+        field: col.id,
+        header: col.displayKey,
+        frozen,
+      };
+    });
   }
 
   formatCurrency(value: number): string {
@@ -56,17 +51,13 @@ export class PTableComponent implements OnInit {
     });
   }
 
-  private loadToggleStates(): void {
-    this.balanceFrozen = JSON.parse(
-      localStorage.getItem('balanceFrozen') || 'false'
-    );
-    this.statusFrozen = JSON.parse(
-      localStorage.getItem('statusFrozen') || 'false'
-    );
-  }
-
-  saveToggleStates(): void {
-    localStorage.setItem('balanceFrozen', JSON.stringify(this.balanceFrozen));
-    localStorage.setItem('statusFrozen', JSON.stringify(this.statusFrozen));
+  toggleFrozenStates(index: number): void {
+    this.columns[index].frozen = !this.columns[index].frozen;
+    let storedFrozenStatus = JSON.parse(localStorage.getItem('frozenStatus') || '{}');
+    let updatedFrozenStatus = {
+      ...storedFrozenStatus,
+      [`${index}`]: this.columns[index].frozen,
+    };
+    localStorage.setItem('frozenStatus', JSON.stringify(updatedFrozenStatus));
   }
 }
